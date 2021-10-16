e126b634f9f555d70d5ea89445682bad3bce37e6
Henrik Doverhill <henrik@doverhill.se>
2021-10-16 23:52:27 +0200

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -1,1 +1,11 @@
+# Build IDL compiler
+dotnet publish .\IDLCompiler\IDLCompiler.csproj -o publish
+
+# Compile interfaces
+cd VFS\IPC
+..\..\publish\IDLCompiler VFS_IDL.json
+cd ..
+cd ..
+
+# Compile everything
 dotnet publish .\ChaosLab.sln -o .\publish\
