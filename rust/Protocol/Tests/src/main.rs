9223cc168736f9eef47f4c2baf5c4a0fa313eb05
Henrik Doverhill <henrik@doverhill.se>
2022-11-21 10:36:49 +0100

    more test

--- rust/Protocol/Tests/src/main.rs
+++ rust/Protocol/Tests/src/main.rs
@@ -36,10 +36,135 @@ mod tests {
         }
     }
 
+    struct StorageObject {
+        name: String,
+        path: String,
+    }
+
+    struct Directory {
+        name: String,
+        path: String,
+    }
+
     struct File {
         name: String,
         path: String,
         size: u64,
+    }
+
+    enum ListResult {
+        Directory(&'static Directory),
+        File(&'static File),
+    }
+
+    impl ListResult {
+        pub const DIRECTORY: usize = 0;
+        pub const FILE: usize = 1;
+        
+        pub fn create_directory_at_address(pointer: *mut u8, name: &str, path: &str) -> usize {
+            unsafe {
+                *(pointer as *mut usize) = Self::DIRECTORY;
+                let pointer = pointer.offset(mem::size_of::<usize>() as isize);
+
+                let size = Directory::create_at_address(pointer, name, path);
+                mem::size_of::<usize>() + size
+            }            
+        }
+
+        pub fn create_file_at_address(pointer: *mut u8, name: &str, path: &str, size: u64) -> usize {
+            unsafe {
+                *(pointer as *mut usize) = Self::FILE;
+                let pointer = pointer.offset(mem::size_of::<usize>() as isize);
+
+                let size = File::create_at_address(pointer, name, path, size);
+                mem::size_of::<usize>() + size
+            }            
+        }
+
+        pub fn get_from_address(pointer: *mut u8) -> (usize, Self) {
+            unsafe {
+                let enum_type = *(pointer as *const usize);
+                let pointer = pointer.offset(mem::size_of::<usize>() as isize);
+
+                let (size, object) = match enum_type {
+                    Self::DIRECTORY => {
+                        let (size, object) = Directory::get_from_address(pointer);
+                        (size, Self::Directory(object))
+                    },
+                    Self::FILE => {
+                        let (size, object) = File::get_from_address(pointer);
+                        (size, Self::File(object))
+                    },
+                    _ => panic!("Unknown enum type")
+                };
+
+                (mem::size_of::<usize>() + size, object)
+            }
+        }
+    }
+
+    impl Directory {
+        pub fn create_at_address(pointer: *mut u8, name: &str, path: &str) -> usize {
+            unsafe {
+                // let object: *mut Directory = mem::transmute(pointer);
+                // no fixed sized fields so above is not needed
+
+                let pointer: *mut u8 = pointer.offset(mem::size_of::<Directory>() as isize);
+
+                let name_length = name.len();
+                *(pointer as *mut usize) = name_length;
+                let pointer = pointer.offset(mem::size_of::<usize>() as isize);
+                core::ptr::copy(name.as_ptr(), pointer, name_length);
+                let pointer = pointer.offset(name_length as isize);
+
+                let path_length = path.len();
+                *(pointer as *mut usize) = path_length;
+                let pointer = pointer.offset(mem::size_of::<usize>() as isize);
+                core::ptr::copy(path.as_ptr(), pointer, path_length);
+                // str_pointer = str_pointer.offset(path_length);
+
+                mem::size_of::<Directory>()
+                    + mem::size_of::<usize>()
+                    + name_length
+                    + mem::size_of::<usize>()
+                    + path_length
+            }
+        }
+
+        pub fn get_from_address(pointer: *mut u8) -> (usize, &'static mut Self) {
+            unsafe {
+                let object: *mut Directory = mem::transmute(pointer);
+
+                let pointer: *mut u8 = pointer.offset(mem::size_of::<Directory>() as isize);
+
+                let name_length = *(pointer as *mut usize);
+                let pointer = pointer.offset(mem::size_of::<usize>() as isize);
+                (*object).name = core::str::from_utf8_unchecked(core::slice::from_raw_parts(
+                    pointer as *const u8,
+                    name_length,
+                ))
+                .to_owned();
+                let pointer = pointer.offset(name_length as isize);
+
+                let path_length = *(pointer as *mut usize);
+                let pointer = pointer.offset(mem::size_of::<usize>() as isize);
+                (*object).path = core::str::from_utf8_unchecked(core::slice::from_raw_parts(
+                    pointer as *const u8,
+                    path_length,
+                ))
+                .to_owned();
+                // str_pointer = str_pointer.offset(name_length);
+
+                (
+                    mem::size_of::<Directory>()
+                        + mem::size_of::<usize>()
+                        + name_length
+                        + mem::size_of::<usize>()
+                        + path_length,
+                    object.as_mut().unwrap(),
+                )
+            }
+        }
     }
 
     impl File {
@@ -71,7 +196,7 @@ mod tests {
             }
         }
 
-        pub fn get_from_address(pointer: *mut u8) -> (usize, &'static mut File) {
+        pub fn get_from_address(pointer: *mut u8) -> (usize, &'static mut Self) {
             unsafe {
                 let object: *mut File = mem::transmute(pointer);
 
@@ -178,6 +303,38 @@ mod tests {
                     object.as_mut().unwrap(),
                 )
             }
+        }
+    }
+
+    #[test]
+    fn test_listresult_create_at_and_get_from() {
+        unsafe {
+            let layout = Layout::from_size_align(512 * 1024, 4 * 1024).expect("Invalid layout");
+            let raw: *mut u8 = mem::transmute(alloc::alloc(layout));
+
+            let file_at = raw;
+            let size = ListResult::create_file_at_address(raw, "test.txt", "//root", 7904);
+            let raw = raw.offset(size as isize);
+            let size = ListResult::create_directory_at_address(raw, "folder", "//root/sub");
+
+            let (file_size, file) = ListResult::get_from_address(file_at);
+            let (dir_size, dir) = ListResult::get_from_address(raw);
+
+            match file {
+                ListResult::File(f) => {
+                    assert_eq!("test.txt", f.name);
+                    assert_eq!("//root", f.path);
+                    assert_eq!(7904, f.size);
+                },
+                _ => panic!("wrong enum type")
+            };
+            match dir {
+                ListResult::Directory(d) => {
+                    assert_eq!("folder", d.name);
+                    assert_eq!("//root/sub", d.path);
+                },
+                _ => panic!("wrong enum type")
+            };
         }
     }
 
