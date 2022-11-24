e695f598f3c18c1c80acfd2600d1836a0a0d0d7f
Henrik Doverhill <henrik@doverhill.se>
2022-11-24 16:56:51 +0100

    failing test

--- rust/Tests/TestConsole/src/main.rs
+++ rust/Tests/TestConsole/src/main.rs
@@ -13,13 +13,64 @@ fn test_get_capabilities_returns() {
         let raw: *mut u8 = mem::transmute(alloc::alloc(layout));
 
         let size_write = GetCapabilitiesReturns::create_at_address(raw, true, 1024, 768, 80, 50);
-        let (size_read, result) = GetCapabilitiesReturns::get_from_address(raw);
+        assert!(size_write > 0);
 
+        let (size_read, result) = GetCapabilitiesReturns::get_from_address(raw);
         assert_eq!(size_write, size_read);
         assert_eq!(true, (*result).is_framebuffer);
         assert_eq!(1024, (*result).framebuffer_size.width);
         assert_eq!(768, (*result).framebuffer_size.height);
         assert_eq!(80, (*result).text_size.width);
         assert_eq!(50, (*result).text_size.height);
+    }
+}
+
+#[test]
+fn test_write_objects_parameters_empty() {
+    unsafe {
+        let layout = Layout::from_size_align(512 * 1024, 4 * 1024).expect("Invalid layout");
+        let raw: *mut u8 = mem::transmute(alloc::alloc(layout));
+
+        let objects: Vec<WriteObjectsParametersObjectsEnum> = Vec::new();
+        let size_write = WriteObjectsParameters::create_at_address(raw, objects);
+        assert!(size_write > 0);
+
+        let (size_read, result) = WriteObjectsParameters::get_from_address(raw);
+        assert_eq!(size_write, size_read);
+        assert_eq!(0, (*result).objects.len());
+    }
+}
+
+#[test]
+fn test_write_objects_parameters_nonempty() {
+    unsafe {
+        let layout = Layout::from_size_align(512 * 1024, 4 * 1024).expect("Invalid layout");
+        let raw: *mut u8 = mem::transmute(alloc::alloc(layout));
+
+        let mut map_field1 = MapField {
+            name: "field".to_string(),
+            value: MapFieldValueEnum::TypeI64(77 as i64)
+        };
+        let mut map = Map { 
+            name: "map".to_string(), 
+            description: "mapdescr".to_string(),
+            fields: vec!(&mut map_field1 as *mut MapField),
+        };
+        let mut table_rows = vec!(&mut map as *mut Map);
+        let mut table = Table {
+            name: "name".to_string(),
+            description: "description".to_string(),
+            columns: vec!("a".to_string(), "b".to_string()),
+            rows: table_rows,
+        };
+        let mut objects: Vec<WriteObjectsParametersObjectsEnum> = Vec::new();
+        objects.push(WriteObjectsParametersObjectsEnum::TypeTable(&mut table as *mut Table));
+        objects.push(WriteObjectsParametersObjectsEnum::TypeMap(&mut map as *mut Map));
+        let size_write = WriteObjectsParameters::create_at_address(raw, objects);
+        assert!(size_write > 0);
+
+        let (size_read, result) = WriteObjectsParameters::get_from_address(raw);
+        assert_eq!(size_write, size_read);
+        assert_eq!(2, (*result).objects.len());
     }
 }
