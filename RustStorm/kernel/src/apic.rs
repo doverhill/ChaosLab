2dfef6b5283681ccdfa2d0ea59a469c91fba245a
Henrik Doverhill <henrik@doverhill.se>
2024-01-10 23:22:49 +0100

    adding new identity_map feature to bootloader

---
+++ RustStorm/kernel/src/apic.rs
@@ -0,0 +1,70 @@
+use acpi::madt::MadtEntry::*;
+use bootloader_api::info::Optional;
+use x2apic::{lapic::LocalApic, *};
+
+use crate::serial_println;
+
+#[derive(Clone)]
+struct Handler();
+
+impl acpi::AcpiHandler for Handler {
+    unsafe fn map_physical_region<T>(&self, physical_address: usize, size: usize) -> acpi::PhysicalMapping<Self, T> {
+        acpi::PhysicalMapping::new(physical_address, core::ptr::NonNull::<T>::new(physical_address as *mut T).unwrap(), size, size, self.clone())
+    }
+
+    fn unmap_physical_region<T>(region: &acpi::PhysicalMapping<Self, T>) {}
+}
+
+pub fn init(rsdp_pointer: Optional<u64>) {
+    serial_println!("Looking for processors");
+
+    // use ACPI to find all processors
+    let mut found_bsp = false;
+    if let Some(rsdp) = rsdp_pointer.as_ref() {
+        serial_println!("Found ACPI RSDP table");
+        unsafe {
+            match acpi::AcpiTables::from_rsdp(Handler(), *rsdp as usize) {
+                Ok(tables) => match tables.find_table::<acpi::madt::Madt>() {
+                    Ok(madt) => {
+                        for entry in madt.entries() {
+                            match entry {
+                                LocalApic(local_apic) => {
+                                    let enabled = local_apic.flags & (1 << 0) != 0;
+                                    serial_println!("Found CPU #{}: enabled={}", local_apic.apic_id, enabled);
+                                    if enabled {
+                                        if !found_bsp {
+                                            serial_println!("Found BSP CPU. Initializing Local APIC");
+                                            found_bsp = true;
+                                        }
+                                        else {
+                                            serial_println!("Starting CPU #{}", local_apic.apic_id);
+                                        }
+                                    }
+                                }
+                                IoApic(io_apic) => serial_println!("Found IO APIC #{}", io_apic.io_apic_id),
+                                LocalApicAddressOverride(address) => {
+                                    let local_apic_address = address.local_apic_address;
+                                    serial_println!("Found 64 bit Local APIC address: {}", local_apic_address)
+                                }
+                                _ => {}
+                            }
+                        }
+                    }
+                    Err(error) => {
+                        serial_println!("Failed to find MADT");
+                    }
+                },
+                Err(error) => {
+                    serial_println!("Failed to parse ACPI tables")
+                }
+            }
+        }
+    }
+}
+
+// unsafe {
+//     match lapic::LocalApicBuilder::new().set_xapic_base(lapic::xapic_base()).build() {
+//         Ok(apic) => serial_println!("is bsp: {}", apic.is_bsp()),
+//         Err(error) => serial_println!("failed to initalize lapic: {}", error),
+//     }
+// }
---
+++ RustStorm/kernel/src/physical.rs
@@ -0,0 +1,109 @@
+use bootloader_api::info::{ MemoryRegions, MemoryRegionKind };
+use spin::Mutex;
+use lazy_static::lazy_static;
+use x86_64::{structures::paging::{PhysFrame, FrameAllocator, Size4KiB, FrameDeallocator}, PhysAddr};
+
+// pub fn init() {
+//     let mut usable: u64 = 0;
+//     let mut total: u64 = 0;
+//     for region in boot_info.memory_regions.iter() {
+//         serial_println!("Memory region {:?}", region);
+//         if region.kind == MemoryRegionKind::Usable {
+//             usable += region.end - region.start;
+//         }
+//         total += region.end - region.start; 
+//     }
+//     serial_println!("{} of {} free", usable, total);
+// }
+
+lazy_static! {
+    pub static ref ALLOCATOR: Mutex<Option<PhysicalFrameAllocator>> = {
+        Mutex::new(None)
+    };
+}
+
+pub fn init(memory_regions: &MemoryRegions) {
+    let mut allocator = ALLOCATOR.lock();
+    *allocator = Some(PhysicalFrameAllocator::init(memory_regions));
+}
+
+struct FreeFrame {
+    next_frame: Option<*mut FreeFrame>,
+    previous_frame: Option<*mut FreeFrame>
+}
+
+unsafe impl Send for FreeFrame {}
+
+pub struct PhysicalFrameAllocator {
+    first_free: Option<*mut FreeFrame>,
+    free_frame_count: u64,
+    used_frame_count: u64
+}
+
+unsafe impl Send for PhysicalFrameAllocator {}
+
+impl PhysicalFrameAllocator {
+    pub fn init(memory_regions: &MemoryRegions) -> Self {
+        // get usable regions from memory map
+        let regions = memory_regions.iter();
+        let usable_regions = regions
+            .filter(|r| r.kind == MemoryRegionKind::Usable);
+        // map each region to its address range
+        let addr_ranges = usable_regions
+            .map(|r| r.start..r.end);
+        // transform to an iterator of frame start addresses
+        let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096));
+
+        let mut frame_count: u64 = 0;
+        let mut previous_frame: Option<*mut FreeFrame> = None;
+        let mut first_free: Option<*mut FreeFrame> = None;
+        for frame in frame_addresses {
+            let free_frame = frame as *mut FreeFrame;
+            unsafe { (*free_frame).previous_frame = previous_frame; }
+            match previous_frame {
+                Some(p) => unsafe { (*p).next_frame = Some(free_frame) },
+                None => first_free = Some(free_frame)
+            };
+            previous_frame = Some(free_frame);
+            frame_count += 1;
+        }
+        match previous_frame {
+            Some(p) => unsafe { (*p).next_frame = None },
+            None => ()
+        }
+
+        PhysicalFrameAllocator { first_free: first_free, free_frame_count: frame_count, used_frame_count: 0 }
+    }
+}
+
+unsafe impl FrameAllocator<Size4KiB> for PhysicalFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        let mut first_free = self.first_free.unwrap();
+        let mut next_frame: *mut FreeFrame;
+        unsafe { next_frame = (*first_free).next_frame.unwrap(); }
+
+        self.first_free = Some(next_frame);
+        unsafe { (*next_frame).previous_frame = None; }
+        self.free_frame_count -= 1;
+        self.used_frame_count += 1;
+
+        Some(PhysFrame::from_start_address(PhysAddr::new(first_free as u64)).unwrap())
+    }
+}
+
+impl FrameDeallocator<Size4KiB> for PhysicalFrameAllocator {
+    unsafe fn deallocate_frame(&mut self, frame: PhysFrame) {
+        let free_frame = frame.start_address().as_u64() as *mut FreeFrame;
+        let old_frame = self.first_free.unwrap();
+
+        unsafe {
+            (*free_frame).previous_frame = None;
+            (*free_frame).next_frame = Some(old_frame);
+            (*old_frame).previous_frame = Some(free_frame);
+        }
+
+        self.first_free = Some(free_frame);
+        self.used_frame_count -= 1;
+        self.free_frame_count += 1;
+    }
+}
